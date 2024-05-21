e04d89218035ac10330fa4ba0a793dfd6574c356
jakapongh <jkphemla@gmail.com>
2024-05-21 11:30:43 +0700

    passes all tests, misunderstood question

--- hw-04/sansprimes.py
+++ hw-04/sansprimes.py
@@ -1,7 +1,7 @@
 # Assignment 4, Task 7
 # Name: Jakapong Hemla
 # Collaborators: None
-# Time Spent: 
+# Time Spent: like 2 hours
 
 
 def is_prime(n: int) -> bool:
@@ -19,32 +19,37 @@ def is_prime(n: int) -> bool:
 def sans_primes(numbers: list[int]) -> list[int]:
     new_list = []
     i = 0
+    previous_is_prime = False
+
     while i < len(numbers):
         number = numbers[i]
-        if is_prime(number):
-            # Skip next num if not at end of list
-            if i + 1 <= (len(numbers) - 1):
-                i += 1
-        else:
+
+        if previous_is_prime:
+            previous_is_prime = is_prime(number)
+            i += 1
+            continue
+
+        if not is_prime(number):
             new_list.append(number)
 
+        previous_is_prime = is_prime(number)
         i += 1
-    print(new_list)
+
     return new_list
 
 
 def test_all():
-    # assert is_prime(1) is False
-    # assert is_prime(2) is True
-    # assert is_prime(-2) is False
-    # assert is_prime(5) is True
-    # assert is_prime(41) is True
-    # assert is_prime(323) is False
-    #
-    # assert sans_primes([1, 4, 9, 10]) == [1, 4, 9, 10]
-    # assert sans_primes([1, 11, 9, 10, 17]) == [1, 10]
-    # assert sans_primes([3, 10, 2, 8, 9, 4, 1, 7, 6, 5, 11]) == [9, 4, 1]
-    # assert sans_primes([3, 1, 2, 4]) == []
+    assert is_prime(1) is False
+    assert is_prime(2) is True
+    assert is_prime(-2) is False
+    assert is_prime(5) is True
+    assert is_prime(41) is True
+    assert is_prime(323) is False
+
+    assert sans_primes([1, 4, 9, 10]) == [1, 4, 9, 10]
+    assert sans_primes([1, 11, 9, 10, 17]) == [1, 10]
+    assert sans_primes([3, 10, 2, 8, 9, 4, 1, 7, 6, 5, 11]) == [9, 4, 1]
+    assert sans_primes([3, 1, 2, 4]) == []
     assert sans_primes([3, -2, 5, 7, 1, 42]) == [42]
     assert sans_primes([1, 0, 3, 0, -2, 5, 7, 1, 42, 9]) == [1, 0, -2, 42, 9]
 
