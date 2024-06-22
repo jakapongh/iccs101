f9bdf1771ad467a33251cbb2dc9a64a7f58cd907
jakapongh <jkphemla@gmail.com>
2024-06-22 15:14:22 +0700

    finish part 1

--- lessons/21/country.py
+++ lessons/21/country.py
@@ -7,9 +7,13 @@ class Country:
         self.area = area
 
     def __str__(self):
-        return f"{self.name}, {str(self.population)}, {str(self.area)}"
+        return f"{self.name} has a population of {str(self.population)} and is {str(self.area)} km^2"
+
+    def __repr__(self):
+        return f"Country(name={self.name}, population={self.population}, area={self.area})"
 
     def is_larger(self, country: Country):
+        # Check that the country passed is actually of type Country
         if not isinstance(country, Country):
             raise TypeError('country is not a country')
 
@@ -24,5 +28,6 @@ canada = Country('Canada', 34482779, 9984670)
 usa = Country('United States of America', 313914040, 9826675)
 print(canada.is_larger(usa))
 print(canada.population_density())
-print(usa)
+print(canada)
+print([canada])
 
