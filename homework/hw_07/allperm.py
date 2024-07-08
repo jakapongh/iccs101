bdb35eec51f0ce821c64f03ce48e186e0ab678b7
Jakapong Hemla <jkphemla@gmail.com>
2024-07-09 00:06:07 +0700

    wip

--- homework/hw_07/allperm.py
+++ homework/hw_07/allperm.py
@@ -2,3 +2,21 @@
 # Name: Jakapong Hemla
 # Collaborators: -
 # Time Spent: -
+
+
+def all_perm(n: int) -> set[tuple[int, ...]]:
+    if n == 1:
+        return {(1,)}
+
+    smaller_perms = all_perm(n - 1)
+    result = set()
+
+    for perm in smaller_perms:
+        for i in range(n):
+            new_perm = perm[:i] + (n,) + perm[i:]
+            result.add(new_perm)
+
+    return result
+
+
+print(all_perm(3))
